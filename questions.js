7080b7861b4b0ff6e6fd8a7be15cbc06fd3e73c3
Leo La <leola90@gmail.com>
2020-01-19 11:06:29 -0600

    intial commit

---
+++ questions.js
@@ -0,0 +1,49 @@
+var questions = [
+  {
+    question: 'What is 2 + 5',
+    answers: [
+      { text: '7', correct: true},
+      { text: '25', correct: false},
+      { text: '3', correct: false},
+      { text: '4', correct: false}
+    ]
+  },
+
+  {
+    question: 'What is 5 + 5',
+    answers: [
+      { text: '6', correct: false},
+      { text: '10', correct: true},
+      { text: '4', correct: false},
+      { text: '5', correct: false}
+    ]
+  },
+
+  {
+    question: 'What is 7 + 5',
+    answers: [
+      { text: '8', correct: false},
+      { text: '26', correct: false},
+      { text: '4', correct: false},
+      { text: '12', correct: true}
+    ]
+  },
+
+  {
+    question: 'What is 9 + 5',
+    answers: [
+      { text: '14', correct: true},
+      { text: '24', correct: false},
+      { text: '2', correct: false},
+      { text: '3', correct: false}
+    ]
+  }
+
+]
+
+  
+
+
+
+
+  
